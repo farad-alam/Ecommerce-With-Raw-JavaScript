32fff81d4d592dc2a27f6df42c24b379535f1214
unknown <farad.dev@gmail.com>
2024-11-09 12:46:44 +0600

    Frontend ready for Watch Ecommerce

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: '5rem',
+      },
+      backgroundImage: {
+        'custom-bg': "url('images/watch-bg.jpg')",
+      },
+    },
+  },
+}
+
+
