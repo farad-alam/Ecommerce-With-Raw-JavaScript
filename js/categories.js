4671cb3b8d506757525da93328cbba9017d96cf6
Farad Alam <farad.dev@gmail.com>
2024-11-10 12:23:28 +0600

    filtered product by categorie

--- js/categories.js
+++ js/categories.js
@@ -1,3 +1,92 @@
+// import {userLogOut} from "./script.js"
+
+let loggedInHtml = `
+<button type="button"
+                            class="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+                            id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
+                            <span class="sr-only">Open user menu</span>
+                            <img class="w-8 h-8 rounded-full" src="https://avatars.githubusercontent.com/u/103895201?v=4" alt="user photo">
+                        </button>
+                        <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
+                            id="user-dropdown">
+                            <div class="px-4 py-3">
+                                <span class="block text-sm text-gray-900 dark:text-white">Bonnie Green</span>
+                                <span class="block text-sm text-gray-500 truncate dark:text-gray-400">name@flowbite.com</span>
+                            </div>
+                            <ul class="py-2" aria-labelledby="user-menu-button">
+                                <li>
+                                    <a href="#"
+                                        class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Dashboard</a>
+                                </li>
+                                <li>
+                                    <a href="#"
+                                        class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Settings</a>
+                                </li>
+                                <li>
+                                    <a href="#"
+                                        class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Earnings</a>
+                                </li>
+                                <li>
+                                    <a id="" href="#"
+                                        class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign
+                                        out</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <!-- Sign Up and Login Buttons -->
+                        <div id="signupLoginButtons">
+                        
+                            <button onclick="userLogOut()" type="button" id="logoutButton" 
+                                class="px-4 py-2 text-sm font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700">Logout</button>
+                        
+                        
+                        </div>
+`
+let loggedOutHtml = `
+<button type="button"
+                            class="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+                            id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
+                            <span class="sr-only">Open user menu</span>
+                            <img class="w-8 h-8 rounded-full" src="https://avatars.githubusercontent.com/u/103895201?v=4" alt="user photo">
+                        </button>
+                        <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
+                            id="user-dropdown">
+                            <div class="px-4 py-3">
+                                <span class="block text-sm text-gray-900 dark:text-white">Bonnie Green</span>
+                                <span class="block text-sm text-gray-500 truncate dark:text-gray-400">name@flowbite.com</span>
+                            </div>
+                            <ul class="py-2" aria-labelledby="user-menu-button">
+                                <li>
+                                    <a href="#"
+                                        class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Dashboard</a>
+                                </li>
+                                <li>
+                                    <a href="#"
+                                        class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Settings</a>
+                                </li>
+                                <li>
+                                    <a href="#"
+                                        class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Earnings</a>
+                                </li>
+                                <li>
+                                    <a href="#"
+                                        class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign
+                                        out</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <!-- Sign Up and Login Buttons -->
+                        <div id="signupLoginButtons">
+                        
+                            <a href="/signup.html"
+                                class="px-4 py-2 text-sm font-medium text-blue-700 bg-white border border-blue-700 rounded-lg hover:bg-blue-700 hover:text-white focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:text-white dark:border-blue-600 dark:hover:bg-blue-700">Sign
+                                Up</a>
+                            <a href="/login.html"
+                                class="px-4 py-2 text-sm font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700">Login</a>
+                        
+                        
+                        </div>
+`
 let products = [
     {
         "title": "Classic Men's Chronograph Watch",
@@ -133,64 +222,89 @@ let products = [
     }
 ]
 
-let singleProduct = `
-<div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <a href="#">
-                <img class="p-8 h-64 mx-auto rounded-t-lg" src="https://m.media-amazon.com/images/I/613pEi4kmyL._AC_SL1500_.jpg"
-                    alt="product image" />
-            </a>
-            <div class="px-5 pb-5">
-                <a href="#">
-                    <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">Apple Watch Series 7 GPS,
-                        Aluminium Case, Starlight Sport</h5>
-                </a>
-                <div class="flex items-center mt-2.5 mb-5">
-                    <div class="flex items-center space-x-1 rtl:space-x-reverse">
-                        <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                            fill="currentColor" viewBox="0 0 22 20">
-                            <path
-                                d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                        </svg>
-                        <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                            fill="currentColor" viewBox="0 0 22 20">
-                            <path
-                                d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                        </svg>
-                        <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                            fill="currentColor" viewBox="0 0 22 20">
-                            <path
-                                d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                        </svg>
-                        <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                            fill="currentColor" viewBox="0 0 22 20">
-                            <path
-                                d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                        </svg>
-                        <svg class="w-4 h-4 text-gray-200 dark:text-gray-600" aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                            <path
-                                d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                        </svg>
-                    </div>
-                    <span
-                        class="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">5.0</span>
-                </div>
-                <div class="flex items-center justify-between">
-                    <span class="text-3xl font-bold text-gray-900 dark:text-white">$599</span>
-                    <a href="#"
-                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add
-                        to cart</a>
-                </div>
-            </div>
-        </div>
-`
-document.addEventListener("DOMContentLoaded", attachProductToHomePage)
+// Check User isAuthenticated
+document.addEventListener("DOMContentLoaded", checkAuthentication)
 
-function attachProductToHomePage(){
+function checkAuthentication(){
+
+    let signUpUserHtml = document.getElementsByClassName("profileWithLogout");
+    let userDetails = JSON.parse(localStorage.getItem("userDetails"))
+
+    if (userDetails && userDetails.isLoggedIn) {      
+        signUpUserHtml[0].insertAdjacentHTML("beforeend", loggedInHtml)
+    } else {
+        signUpUserHtml[0].insertAdjacentHTML("beforeend", loggedOutHtml)
+    }
+}
+
+
+
+function userLogin(event){
+    event.preventDefault()
+    const loginForm = document.getElementById("login-form")
+    const userDetails = JSON.parse(localStorage.getItem("userDetails"))
+    console.log(loginForm.username)
+    console.log(userDetails)
+
+    if (userDetails && userDetails.userName){
+        if (userDetails.userName === loginForm.username.value && userDetails.password === loginForm.password.value) {
+            userDetails.isLoggedIn = true
+            localStorage.setItem("userDetails", JSON.stringify(userDetails))
+            window.location.href = "/"
+
+            console.log("user successfully logged in")
+    
+        }
+    } else {
+        console.log("User with these username did not exist")
+    }
+}
+
+function userLogOut(){
+    let userDetails = JSON.parse(localStorage.getItem("userDetails"))
+    console.log(userDetails)
+    if (userDetails && userDetails.userName) {
+        userDetails.isLoggedIn = false
+        localStorage.setItem("userDetails", JSON.stringify(userDetails));
+        console.log("User successfully Logout")
+        // window.location.href = "/login.html";
+        window.location.href = "/login.html"
+        
+    } else {
+        console.log("Can not proces Logout")
+    }
+}
+
+
+document.addEventListener("DOMContentLoaded", showCategoriesProduct)
+
+function showCategoriesProduct(){
+    let urlsearchParm = new URLSearchParams(window.location.search)
+    const categoriesName = urlsearchParm.get("category")
+    insetCategoriTitle(categoriesName)
+    // console.log(categoriesName)
+    let filteredProducts = filterProductsBasedOnCategories(products, categoriesName)
+    // console.log(filteredProducts)
+    let productHtml = genarateProductHTMLFromProductList(filteredProducts)
+
     const displayProductSection = document.getElementById("displayProduct")
-    let displayProductsWithHTML = ''
 
-    products.forEach((product, index, array) => {
+    displayProductSection.insertAdjacentHTML("beforeend", productHtml)
+
+
+}
+
+
+function insetCategoriTitle(categoriesName){
+    document.getElementById("displayCategoryTitle").innerHTML = `Products For ${categoriesName}`
+}
+
+
+
+function genarateProductHTMLFromProductList(productList){
+
+    let displayProductsWithHTML = ''
+    productList.forEach((product, index) => {
             displayProductsWithHTML += `
             <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                         <a href="/productDetails.html?productIndex=${index}">
@@ -243,8 +357,16 @@ function attachProductToHomePage(){
             `
          
     })
-
-    displayProductSection.insertAdjacentHTML("beforeend", displayProductsWithHTML)
+    return displayProductsWithHTML
+   
 
 }
 
+function filterProductsBasedOnCategories(products, categories){
+    let productsl = products.filter(product =>{
+        
+       return product.categories === categories
+    })
+    console.log(productsl)
+    return productsl
+}
